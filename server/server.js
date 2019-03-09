3b8c7b7ce6db635966c34799a319cb4dc1a52f2f
myapos <myapos@yahoo.com>
2019-03-09 17:45:20 +0200

    used babel node to express

--- server/server.js
+++ server/server.js
@@ -1,22 +1,25 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+import express from 'express';
+import http from 'http';
+import io from 'socket.io';
+import { SERVER_PORT } from '../constants/common';
 
-var port = 3000;
+const app = express();
+const http_ = http.Server(app);
+const io_ = io(http_);
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/staticFiles/index.html');
 });
 
-io.on('connection', function (socket) {
+io_.on('connection', function (socket) {
   socket.on('chat message', function (msg) {
     console.log('message: ' + msg);
-    io.emit('chat message', msg);
+    io_.emit('chat message', msg);
   });
 });
 
-io.emit('some event', { for: 'everyone' });
+io_.emit('some event', { for: 'everyone' });
 
-http.listen(port, function () {
-  console.log(`listening on http://localhost:${port}`);
+http_.listen(SERVER_PORT, function () {
+  console.log(`listening on http://localhost:${SERVER_PORT}`);
 });
