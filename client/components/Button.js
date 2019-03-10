55aa4cd65934396d12b997b9915c28e5818e9477
myapos <myapos@yahoo.com>
2019-03-10 16:20:12 +0200

    added enter functionality and button click handling

--- client/components/Button.js
+++ client/components/Button.js
@@ -4,14 +4,24 @@ import { connect } from 'react-redux';
 import * as actions from '../store/actions';
 
 const handleMsg = (props) => {
-  const { data, socket } = props;
+
+  const { data, socket, receivedData } = props;
   socket.emit('chat message', data);
-  console.log('sending', data);
+
+  //check socket callbacks to avoid multiple listeners and memory leaks
+
+  if (!socket._callbacks['$chat message'] || socket._callbacks['$chat message'].length < 1) {
+    socket.on('chat message', function (msg) {
+      receivedData(msg);
+    });
+  }
+
+  // console.log('sending', data);
 }
 
 const Button = (props) => <div
   className="myBtn"
-  onClick={handleMsg(props)}>
+  onClick={() => handleMsg(props)}>
   Send
   </div>
 
--- client/components/Entry.js
+++ client/components/Entry.js
@@ -1,25 +1,41 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Button from '../components/Button';
 import * as actions from '../store/actions';
 
 const handleChange = (e, props) => {
-  console.log('e:', e.target.value);
-
-  const { socket, receivedData, entryChange } = props;
+  // console.log('e:', e.target.value);
+  const { entryChange } = props;
   entryChange(e.target.value);
-  socket.emit('chat message', e.target.value);
-
-  socket.on('chat message', function (msg) {
-    // console.log('received:', msg);
-    receivedData(msg);
-  });
 }
 
-const Entry = (props) => <div>
+const handleKeyPress = (e, props) => {
+  if (e.key.match(/enter/ig)) {
+    console.log('pressed enter');
+
+    const { data, socket, receivedData } = props;
+    socket.emit('chat message', data);
+
+    //check socket callbacks to avoid multiple listeners and memory leaks
+
+    if (!socket._callbacks['$chat message'] || socket._callbacks['$chat message'].length < 1) {
+      socket.on('chat message', function (msg) {
+        receivedData(msg);
+      });
+    }
+
+  }
+
+}
+const Entry = (props) => <div className="entryWrapper">
   <input
     className="entry"
     onChange={e => handleChange(e, props)}
+    onKeyPress={e => handleKeyPress(e, props)}
     placeholder="type your message"></input>
+  <Button
+    className="button"
+    socket={props.socket} />
 </div>
 
 
