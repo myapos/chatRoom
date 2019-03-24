b039ebedabce99a4c0c92c3afb6840cf137a371a
myapos <myapos@yahoo.com>
2019-03-24 21:25:06 +0200

    logs

--- constants/common.js
+++ constants/common.js
@@ -6,6 +6,7 @@
 
 const common = {};
 
+console.log('port from common', process.env.PORT);
 common.CLIENT_PORT = process.env.NODE_ENV === 'development' ? 1234 : 2222;
 common.SERVER_PORT = process.env.PORT || 3000;
 common.BASE_URL = process.env.NODE_ENV === 'development'
