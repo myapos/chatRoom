0594756ea6c4f7fb5d128f6021fd00cf9f930884
myapos <myapos@yahoo.com>
2019-03-24 11:52:13 +0200

    setup common variables to dev webpack and server

--- constants/common.js
+++ constants/common.js
@@ -1,5 +1,16 @@
-export const SERVER_PORT = process.env.PORT ? 5000 : 3000;
-export const BASE_URL = process.env.PORT
+// export const SERVER_PORT = process.env.PORT ? 5000 : 3000;
+
+// export const CLIENT_PORT = process.env.PORT ? 2222 : 1234;
+// export const BASE_URL = process.env.PORT
+//   ? 'https://chatroomsimpledemo.herokuapp.com/' : 'http://localhost';
+
+// export const dummyMsg = 'test';
+
+const common = {};
+
+common.CLIENT_PORT = process.env.PORT ? 2222 : 1234;
+common.SERVER_PORT = process.env.PORT ? 5000 : 3000;
+common.BASE_URL = process.env.PORT
   ? 'https://chatroomsimpledemo.herokuapp.com/' : 'http://localhost';
 
-export const dummyMsg = 'test';
+module.exports = common;
